b737d5d36854e16f130371e9076ef664fb7d7690
grzesiek <g.pierzakowski@gmail.com>
2020-08-09 13:17:45 +0200

    templates

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    public function loginAction(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    public function registerAction(): Response
+    {
+
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class UserController extends AbstractController
+{
+    public function loginAction(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    public function registerAction(): Response
+    {
+
+    }
+
+    public function listAction(): Response
+    {
+
+    }
+
+    public function enableAction(int $user): Response
+    {
+
+    }
+}
