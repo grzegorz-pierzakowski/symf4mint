470629f5f2821a7eb3c019e591c0a6d4d452db12
grzesiek <g.pierzakowski@gmail.com>
2020-09-05 18:35:27 +0200

    tree

--- src/Command/TreeCommand.php
+++ src/Command/TreeCommand.php
@@ -8,17 +8,21 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\Filesystem\Filesystem;
 
 final class TreeCommand extends Command implements ContainerAwareInterface
 {
     use ContainerAwareTrait;
 
+    private $names = [];
+
     protected function configure(): void
     {
+        $targetDir = __DIR__ . '/../../var/input/';
         $this->setName('app:tree:fill')
-            ->addArgument('treeFilePath', InputArgument::REQUIRED, 'tree file')
-            ->addArgument('listFilePath', InputArgument::REQUIRED, 'list file')
-            ->addArgument('resultFilePath', InputArgument::OPTIONAL, 'outputFile');
+            ->addArgument('treeFilePath', InputArgument::OPTIONAL, 'tree file', $targetDir . 'tree.json')
+            ->addArgument('listFilePath', InputArgument::OPTIONAL, 'list file', $targetDir . 'list.json')
+            ->addArgument('resultFilePath', InputArgument::OPTIONAL, 'outputFile', __DIR__ . '/../../var/output/list.json');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -26,20 +30,35 @@ final class TreeCommand extends Command implements ContainerAwareInterface
         $tree = $input->getArgument('treeFilePath');
         $list = $input->getArgument('listFilePath');
         $tree = json_decode(file_get_contents($tree), true);
-        $list = json_decode(file_get_contents($list), true);
-        $this->process($tree, $list);
-        $out = $input->getArgument('resultFilePath') ?: $this->container->getParameter('kernel.project_dir') . '/var/out.json';
+        foreach (json_decode(file_get_contents($list), true) as $item) {
+            $this->names[$item['category_id']] = $item['translations']['pl_PL']['name'];
+        }
+        $tree = $this->process($tree);
+        $out = $input->getArgument('resultFilePath');
+        $this->mkDir($out);
         file_put_contents($out, json_encode($tree));
+        return 0;
     }
 
-    private function process(array &$tree, array $list): void
+    private function process(array $tree): array
     {
-        foreach ($tree as $node) {
-            if (isset($node['children'])) {
-                $this->process($node['children']);
-            } else {
-                //TODO to miało coś dpisać
+        foreach ($tree as $key => $node) {
+            if (array_key_exists($node['id'], $this->names)) {
+                $tree[$key]['name'] = $this->names[$node['id']];
+            }
+            if (!empty($node['children'])) {
+                $tree[$key]['children'] = $this->process($node['children']);
             }
         }
+        return $tree;
+    }
+
+    private function mkDir(string $file): void
+    {
+        $parts = explode('/', $file);
+        array_pop($parts);
+        $directory = implode('/', $parts);
+        if (file_exists($directory)) return;
+        (new Filesystem())->mkdir($directory);
     }
 }
